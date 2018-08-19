9516b3413407281f62517aa35ca59356d341a7f1
Demetrios Kalcassinas <disinas@icloud.com>
2018-08-19 17:39:28 +0300

    feat: display infowindow on marker click
    Info-windows is Google mas API method for creating information boxes on screen that shows interesting text and images related to the spot on map that is attatched. Every info-window is popup above the map at a given location. In this application one info-window is created and on click of a marker a box with content is setting an info-window.

--- src/App.js
+++ src/App.js
@@ -26,12 +26,13 @@ class App extends Component {
       client_secret: "0OPDP0NNJ3MI3XX4JWUZAPR4IUFYDGIF2UYCQBPXLVYUUE5V",
       ll: "37.9726543,23.7263274",
       query: "food",
+      limit: "18",
       v: "20180323"
     }
 
     axios.get(endPoint + new URLSearchParams(params))
       .then(response => {
-        console.log(response.data.response.groups[0].items); //dev test
+        // console.log(response.data.response.groups[0].items); //dev test
         this.setState({
           venues: response.data.response.groups[0].items
         }, this.mapFetch()); //fetch map and render it after storing venues state
@@ -43,19 +44,27 @@ class App extends Component {
 
   initMap =  () => {
 
-    var mapAthens = new window.google.maps.Map(document.getElementById('map'), {
+    var map = new window.google.maps.Map(document.getElementById('map'), {
       center: { lat: 37.9726543, lng: 23.7263274 },
       zoom: 16
-    });
+    }) //create map of the specified area
     
+    var infowindow = new window.google.maps.InfoWindow() //create infowindow
+
     this.state.venues.map(venueAll => {
 
+      var contentString = `${venueAll.venue.name}`;
+      // var contentString = `${venueAll.venue.address}`;
       var marker = new window.google.maps.Marker({
         position: {lat: venueAll.venue.location.lat , lng: venueAll.venue.location.lng},
-        map: mapAthens,
+        map: map,
         title: venueAll.venue.name
-      })
+      }) //create marker for venue
 
+      marker.addListener('click', function() {
+        infowindow.setContent(contentString)
+        infowindow.open(map, marker)
+      }) //open the infowindow on click
     })
   
 
